1dc38cee7e470c159d6fc22a91e294cb3a674b9d
Prem Gandhi <phgandh2@ncsu.edu>
2025-09-09 10:09:46 -0400

    refactor

--- HW1/src/boid.cpp
+++ HW1/src/boid.cpp
@@ -14,78 +14,42 @@ void Boid::update()
     {
     case 0:
         m_sprite.move(sf::Vector2f(m_speed, 0.f));
-        if (m_sprite.getPosition().x >= 640 and m_sprite.getPosition().y <= 0)
-        {
+        if (m_sprite.getPosition().x >= 640 and m_sprite.getPosition().y <= 0){
             m_spriteState = 1; // moving down
             m_sprite.setRotation(90.f);
         }
         break;
     case 1:
         m_sprite.move(sf::Vector2f(0.f, m_speed));
-        if (m_sprite.getPosition().x >= 640 and m_sprite.getPosition().y >= 480)
-        {
+        if (m_sprite.getPosition().x >= 640 and m_sprite.getPosition().y >= 480){
             m_spriteState = 2; // moving left
             m_sprite.setRotation(180.f);
         }
         break;
     case 2:
         m_sprite.move(sf::Vector2f(-m_speed, 0.f));
-        if (m_sprite.getPosition().x <= 0 and m_sprite.getPosition().y >= 480)
-        {
+        if (m_sprite.getPosition().x <= 0 and m_sprite.getPosition().y >= 480){
             m_spriteState = 3; // moving up
             m_sprite.setRotation(270.f);
         }
         break;
     case 3:
         m_sprite.move(sf::Vector2f(0.f, -m_speed));
-        if (m_sprite.getPosition().x <= 0 and m_sprite.getPosition().y <= 0)
-        {
+        if (m_sprite.getPosition().x <= 0 and m_sprite.getPosition().y <= 0){
             m_spriteState = 4; // stopped
             m_sprite.setRotation(0.f);
         }
         break;
     case 4:
-        if (m_sprite.getPosition().x <= 0 and m_sprite.getPosition().y <= 0)
-        {
+        if (m_sprite.getPosition().x <= 0 and m_sprite.getPosition().y <= 0){
             m_spriteState = 4; // stopped
             m_sprite.setRotation(0.f);
             m_stopped = true;
         }
         break;
     default:
-
         break;
     }
-    // // Determine the sprite state based on its position and the current state.
-    // if (m_sprite.getPosition().x < 640 and m_sprite.getPosition().y <= 0 and m_spriteState == 0){
-    //     m_spriteState = 0; // moving right
-    //     m_sprite.setRotation(0.f);
-    // } else if (m_sprite.getPosition().x >= 640 and m_sprite.getPosition().y <= 0 and m_spriteState == 0){
-    //     m_spriteState = 1; // moving down
-    //     m_sprite.setRotation(90.f);
-    // } else if (m_sprite.getPosition().x >= 640 and m_sprite.getPosition().y >= 480 and m_spriteState == 1){
-    //     m_spriteState = 2; // moving left
-    //     m_sprite.setRotation(180.f);
-    // } else if (m_sprite.getPosition().x <= 0 and m_sprite.getPosition().y >= 480 and m_spriteState == 2){
-    //     m_spriteState = 3; // moving up
-    //     m_sprite.setRotation(270.f);
-    // } else if (m_sprite.getPosition().x <= 0 and m_sprite.getPosition().y <= 0 and m_spriteState == 3){
-    //     m_spriteState = 4; // stopped
-    //     m_sprite.setRotation(0.f);
-    // }
-
-    // // Update the sprite position based on its state.
-    // if (m_spriteState == 0){ // moving right
-    //     m_sprite.move(sf::Vector2f(m_speed, 0.f));
-    // } else if (m_spriteState == 1){ // moving down
-    //     m_sprite.move(sf::Vector2f(0.f, m_speed));
-    // } else if (m_spriteState == 2){ // moving left
-    //     m_sprite.move(sf::Vector2f(-m_speed, 0.f));
-    // } else if (m_spriteState == 3){ // moving up
-    //     m_sprite.move(sf::Vector2f(0.f, -m_speed));
-    // } else if (m_spriteState == 4){ // stopped
-    //     m_stopped = true;
-    // }
 }
 
 void Boid::draw(sf::RenderWindow &window)
