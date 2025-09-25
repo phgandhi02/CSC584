85ace52b8c48530e3939a66d7d27b2977e766ef1
Prem Gandhi <phgandhi02@gmail.com>
2025-09-24 23:03:32 -0400

    implemented logic for boids algorithm for main.cpp

--- src/HW2/app/main.cpp
+++ src/HW2/app/main.cpp
@@ -1,10 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include "../include/boid.hpp"
+#include "../include/algorithms/boids.hpp"
+
+#include <memory>
+#include <iostream>
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+    // Construct the boid sprites and pass the boid object as a unique_ptr to the vector to store the collection. 
+    std::vector<std::unique_ptr<Boid>> boids;
+    for (int i = 0; i < 50; i++){
+        boids.push_back(std::make_unique<Boid>()); // append the boid to the boids vector as a unique_ptr to ensure memory is shared efficiently.
+    }
+    // Create the Boids Algorithm object to control the movement behavior for the boids in an easy interface. 
+    BoidsAlgorithm boids_algorithm = BoidsAlgorithm(boids,1.0f,1.0f,1.0f); 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -13,6 +25,11 @@ int main()
             {
                 window.close();
             }
+        }
+
+        for (auto& boidPtr: boids){
+            // Update the boids states using the existing boids algorithm params.
+            boids_algorithm.update();
         }
 
         window.clear();
