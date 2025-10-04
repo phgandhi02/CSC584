a09d81eee4aafde94191190ad1dd64e592c95034
Prem Gandhi <phgandhi02@gmail.com>
2025-10-04 17:59:18 -0400

    feat(steering): implement wander behavior

--- src/HW2/include/steering/kinematic_wander.hpp
+++ src/HW2/include/steering/kinematic_wander.hpp
@@ -7,7 +7,15 @@ class KinematicWander: public virtual KinematicMovement {
     public:
         KinematicWander() = default;
         ~KinematicWander() = default;
+        float sampleDifference();
         KinematicSteeringOutput getSteering(Static& character) override;
+    private:
+        float m_rotation;
+        int m_framesSinceSample;
+
+        int m_samplingInterval;
+        float m_maxRotation;
+        float m_maxSpeed;
 };
 
 #endif // KinematicWander_HPP
--- src/HW2/src/steering/kinematic_wander.cpp
+++ src/HW2/src/steering/kinematic_wander.cpp
@@ -1,5 +1,29 @@
 #include "../../include/steering/kinematic_wander.hpp"
 
+#include <random>
+
+float KinematicWander::sampleDifference()
+{
+    static std::default_random_engine rand_gen;
+    std::uniform_real_distribution<> dis(0.0f, 1.0f);
+    float randomValue = dis(rand_gen) - dis(rand_gen);
+    return randomValue;
+};
+
 KinematicSteeringOutput KinematicWander::getSteering(Static& character){
-    return KinematicSteeringOutput();
+   if (0 == m_framesSinceSample){
+        m_rotation = sampleDifference() * m_maxRotation;
+    }
+    m_framesSinceSample = (m_framesSinceSample + 1) % m_samplingInterval;
+
+    KinematicSteeringOutput result = KinematicSteeringOutput();
+    
+    // Get velocity from the vector form of the orientation
+    auto newVelocity = sf::Vector2f(cos(character.getOrientation().asRadians()) * m_maxSpeed, -1.0f * sin(character.getOrientation().asRadians()) * m_maxSpeed);
+    result.setVelocity(newVelocity); // set velocity in the direction of the character's orientation
+
+    // Change our orientation randomly
+    result.setRotation(sf::radians(m_rotation));
+    
+    return result;
 };
