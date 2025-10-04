3ec762463007f0edebbe640599eedc6e61970771
Prem Gandhi <phgandhi02@gmail.com>
2025-10-04 19:57:32 -0400

    fix(steering): corrected kinematic align to use kinematic calc instead of dynamic

--- src/HW2/include/steering/kinematic_align.hpp
+++ src/HW2/include/steering/kinematic_align.hpp
@@ -10,15 +10,7 @@ class KinematicAlign: public virtual KinematicMovement {
         KinematicSteeringOutput getSteering(Static& character) override;
         void updateTarget(const Static targetPos);
     private:
-        float m_smoothing = 0.0f;
-        sf::Angle m_maxAngularAcceleration = sf::degrees(30.0f); // Maximum angular acceleration
-        sf::Angle m_maxRotation = sf::degrees(20.0f); // Maximum rotation speed
-
-        sf::Angle m_rotationSize = sf::Angle(sf::degrees(0.1f)); // Threshold to consider aligned
-        sf::Angle m_targetRadius = sf::degrees(5.0f); // Radius for arriving at target orientation
-        sf::Angle m_slowRadius = sf::degrees(30.0f); // Radius for beginning to slow down
-
-        sf::Angle m_timeToTarget = sf::degrees(0.1f); // Time over which to achieve target speed
+        float m_smoothing = 1.0f; // const between 0-1
 };
 
 #endif // KinematicAlign_HPP
--- src/HW2/src/steering/kinematic_align.cpp
+++ src/HW2/src/steering/kinematic_align.cpp
@@ -1,56 +1,19 @@
 #include "../../include/steering/kinematic_align.hpp"
 
-KinematicSteeringOutput KinematicAlign::getSteering(Static& character){
-    // checking m_target to make sure the
+KinematicSteeringOutput KinematicAlign::getSteering(Static &character)
+{
+  // checking m_target to make sure the
   if (target.null_output == true)
   {
     auto result = KinematicSteeringOutput();
     result.null_output = true;
     return result;
   }
-  else
-  {
-    KinematicSteeringOutput result = KinematicSteeringOutput();
-    sf::Angle direction = target.getOrientation() - character.getOrientation();
+  auto direction = target.getPosition() - character.getPosition();
+  character.setOrientationFloat(getNewOrientation(character.getOrientationFloat(), direction, m_smoothing));
+  // Linear velocity is zero
 
-    direction = direction.wrapSigned();
-    m_rotationSize = sf::radians(std::abs(direction.asRadians()));
-
-    // Check if we are there, return no steering.
-    if (m_rotationSize < m_targetRadius)
-    {
-      auto result = KinematicSteeringOutput();
-    result.null_output = true;
-    return result;
-    }
-
-    // If we are outside the slow radius, then use maximum rotation
-    if (m_rotationSize > m_slowRadius)
-    {
-      result.setRotation(m_maxRotation);
-    }
-    else
-    { // Otherwise calculate a scaled rotation
-      float rotationSpeed = m_maxRotation.asRadians() * m_rotationSize.asRadians();
-      sf::Angle targetRotation = sf::radians(rotationSpeed / m_slowRadius.asRadians());
-      result.setRotation(targetRotation);
-    }
-
-    // The final target rotation combines speed (already in the varaible) and direction
-    result.setRotation(result.getRotation() * direction.asRadians() / m_rotationSize.asRadians());
-
-    // Acceleration tries to get to the target rotation
-    result.setRotation(sf::radians((result.getRotationFloat() - character.getOrientationFloat()) / m_timeToTarget.asRadians()));
-
-    // Check if the acceleration is too great
-    float angularAcceleration = std::abs(result.getRotationFloat());
-    if (angularAcceleration > m_maxAngularAcceleration.asRadians())
-    {
-      result.setRotation(sf::radians((result.getRotationFloat() / angularAcceleration) * m_maxAngularAcceleration.asRadians()));
-    }
-
-    // Linear velocity is zero
-
-    return result;
-  }
+  auto result = KinematicSteeringOutput();
+  result.null_output = true;
+  return result;
 };
