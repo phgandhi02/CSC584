cb5c1d88320351811e7ef59dbb400b511b15b1a9
Prem Gandhi <phgandhi02@gmail.com>
2025-10-04 15:03:18 -0400

    feat(align): declared and implemented vars and funcs for KinematicArrive based on new Static and KinematicSteeringOutput classes with null_output public prop.

--- src/HW2/include/steering/kinematic_arrive.hpp
+++ src/HW2/include/steering/kinematic_arrive.hpp
@@ -7,7 +7,14 @@ class KinematicArrive: public virtual KinematicMovement {
     public:
         KinematicArrive() = default;
         ~KinematicArrive() = default;
+        void updateTarget(Static targetPos);
         KinematicSteeringOutput getSteering(Static& character) override;
+    private:
+        Static m_target;
+        float m_maxSpeed = 250.0f;
+        float m_smoothing = 0.0f;
+        float m_timeToTarget;
+        float m_stopRadius;
 };
 
 #endif // KinematicArrive_HPP
--- src/HW2/src/steering/kinematic_arrive.cpp
+++ src/HW2/src/steering/kinematic_arrive.cpp
@@ -1,5 +1,50 @@
 #include "../../include/steering/kinematic_arrive.hpp"
 
-KinematicSteeringOutput KinematicArrive::getSteering(Static& character){
-    return KinematicSteeringOutput();
+void KinematicArrive::updateTarget(Static targetPos)
+{
+    m_target = targetPos;
+};
+
+KinematicSteeringOutput KinematicArrive::getSteering(Static &character)
+{
+    // checking m_target to make sure the
+    if (m_target.null_output == true)
+    {
+        auto result = KinematicSteeringOutput();
+        result.null_output = true;
+        return result;
+    }
+    else
+    {
+        updateTarget(m_target);
+        KinematicSteeringOutput result = KinematicSteeringOutput();
+
+        result.setVelocity(m_target.getPosition() - character.getPosition());
+
+        // Check if we are there, if so, return no steering
+        if (result.getVelocity().length() < m_stopRadius)
+        {
+            auto result = KinematicSteeringOutput();
+            result.null_output = true;
+            return result;
+        }
+        float targetSpeed = 0.0f;
+
+        // We need to move to our target, we'd like to get there in in timeToTarget seconds
+        result.setVelocity(result.getVelocity() / m_timeToTarget);
+
+        // Check if the velocity is too fast, if so clip speed
+        if (result.getVelocity().length() > m_maxSpeed)
+        {
+            result.setVelocity(result.getVelocity().normalized() * m_maxSpeed);
+        }
+
+        // Face in the direction we want to move
+        sf::Angle newOrientation = sf::radians(getNewOrientation(character.getOrientation().asRadians(), result.getVelocity(), m_smoothing));
+        character.setOrientation(newOrientation);
+
+        // No angular acceleration
+
+        return result;
+    }
 };
