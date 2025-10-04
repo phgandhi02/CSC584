4c86a1bc5cb968de2764995777d9e345a9ff0d12
Prem Gandhi <phgandhi02@gmail.com>
2025-10-04 15:01:56 -0400

    fix(testing): removed old tests based on past implementation of boids and steering behavior assignment

--- src/HW2/tests/test_main.cpp
+++ src/HW2/tests/test_main.cpp
@@ -19,26 +19,6 @@ TEST(BoidTesting, InitializeBoid)
     EXPECT_EQ(boid.breadcrumbs_on, true);
 };
 
-// Test initialization of a single boid with Align behavior
-TEST(BoidTesting, InitializeBoidAlign)
-{
-    // create a texture object to load the boid image.
-    sf::Texture texture;
-    if (!texture.loadFromFile("../assets/boid-sm.png"))
-    {
-        std::cerr << "Asset didn't load properly" << std::endl;
-    }; // Load texture
-
-    Boid boid(texture, 250.0f, sf::Vector2f(0.0f, 0.0f));
-
-    auto align_behavior = std::make_unique<KinematicAlign>();
-    auto steeringOutput = boid.align(std::move(align_behavior));
-    auto defaultSteeringOutput = KinematicSteeringOutput();
-    bool test = steeringOutput.operator==(defaultSteeringOutput);
-
-    EXPECT_TRUE(test);
-};
-
 class BoidTest : public testing::Test
 {
 protected:
@@ -75,9 +55,24 @@ TEST_F(BoidTest, BoidTextureInitialization)
 TEST_F(BoidTest, InitializeAlign)
 {
     auto align_behavior = std::make_unique<KinematicAlign>();
-    steeringOutput = boid.align(std::move(align_behavior));
+    boid.m_controller = std::move(align_behavior);
+    boid.update(0.01f);
+    KinematicSteeringOutput steeringOutput = boid.getSteering();
     auto defaultSteeringOutput = KinematicSteeringOutput();
-    bool test = steeringOutput.operator==(defaultSteeringOutput);
+    bool test = (steeringOutput == defaultSteeringOutput);
 
     EXPECT_TRUE(test);
 }
+
+TEST_F(BoidTest, InitializeArrive)
+{
+    auto align_behavior = std::make_unique<KinematicAlign>();
+    boid.m_controller = std::move(align_behavior);
+    boid.update(0.01f);
+    KinematicSteeringOutput steeringOutput = boid.getSteering();
+    auto defaultSteeringOutput = KinematicSteeringOutput();
+    bool test = (steeringOutput == defaultSteeringOutput);
+
+    EXPECT_TRUE(test);
+}
+
