41c357c7b13d6e04f1f7d0e1cdf078bba268f7c1
Prem Gandhi <phgandhi02@gmail.com>
2025-10-04 16:15:35 -0400

    feat(input): implemented basic functions for input handling from mouse

--- src/HW2/src/utils.cpp
+++ src/HW2/src/utils.cpp
@@ -1,4 +1,18 @@
 #include "../include/utils.hpp"
-bool InputHandler::wasMouseButtonPressed(){
-    return m_isButtonDown && !m_isPrevFrameButtonDown;
-}
+bool InputHandler::isMouseButtonDown(){
+    return sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
+};
+
+Static InputHandler::update(){
+    isMouseButtonDown();
+    getMousePosition();
+    Static target;
+    if (isMouseButtonDown()){
+        auto targetPos = sf::Vector2f(getMousePosition().x, getMousePosition().y);
+        target = Static(targetPos,sf::degrees(0.f));
+    } else {
+        target = Static();
+        target.null_output = true;
+    }
+    return target; 
+};
