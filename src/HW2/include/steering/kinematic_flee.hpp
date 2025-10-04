b332c9062af489ad0bd6452e8fa83ba5552df147
Prem Gandhi <phgandhi02@gmail.com>
2025-10-04 17:57:30 -0400

    feat(steering): implement flee steering behavior

--- src/HW2/include/steering/kinematic_flee.hpp
+++ src/HW2/include/steering/kinematic_flee.hpp
@@ -8,6 +8,9 @@ class KinematicFlee: public virtual KinematicMovement {
         KinematicFlee() = default;
         ~KinematicFlee() = default;
         KinematicSteeringOutput getSteering(Static& character) override;
+    private:
+        float m_maxSpeed = 250.0f;
+        float m_smoothing = 0.01f;
 };
 
 #endif // KinematicFlee_HPP
