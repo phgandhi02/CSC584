3ee2c847a29aea1a46a664310860d286f266cb12
Prem Gandhi <phgandhi02@gmail.com>
2025-10-04 17:08:39 -0400

    feat(boid): modified boid to have testing_mode bool for correct testing setup

--- src/HW2/src/boid.cpp
+++ src/HW2/src/boid.cpp
@@ -20,16 +20,17 @@ Boid::Boid(const sf::Texture& texture, float speed, sf::Vector2f startPos)
 // Main function to run in the game loop. Updates boid m_character based on steering behavior pointed to m_controller.
 void Boid::update(float dt)
 {
-    // Assign target from mouse click using input handler object.
-    auto target = m_inputHandler->update(); // update will return a Static object with null_output false;
-    
     // Check if boid has a valid steering behavior attached.
     if (m_controller == nullptr){
         return; // if it doesn't have a valid steering behavior then return and don't update m_character.
     }
 
-    // set the target for the controller equal to the target from the input handler (ie. mouse)
-    m_controller->target = target;
+    if (!unittesting){
+        // Assign target from mouse click using input handler object.
+        auto target = m_inputHandler->update(); // update will return a Static object with null_output false;
+        // set the target for the controller equal to the target from the input handler (ie. mouse)
+        m_controller->target = target;
+    }
 
     // Get the steering behavior to return how the boid should move based on the target.
     m_steering = m_controller->getSteering(m_character);
@@ -51,11 +52,6 @@ void Boid::update(float dt)
         m_sprite.setPosition(m_character.getPosition());
         m_sprite.setRotation(m_character.getOrientation() + sf::degrees(90.0f)); //AI-gen: Adjust for sprite facing up
     }
-};
-
-// Helper function to get the Boid's current position
-sf::Vector2f Boid::getPosition(){
-    return m_character.getPosition();
 };
 
 // Helper function to set the Boid texture
