4f28ea7489f689b75a86e51116104845897155be
Prem Gandhi <phgandhi02@gmail.com>
2025-10-05 00:25:00 -0400

    fix(input handling): InputHandler sends mouse position relative to window instead of display. Steering behaviors now behaving correctly.

--- src/HW2/include/utils.hpp
+++ src/HW2/include/utils.hpp
@@ -5,13 +5,18 @@
 
 #include "./steering_behavior.hpp"
 
+#include <memory>
+
 class InputHandler{
     public:
+        InputHandler(sf::RenderWindow& window);
+        ~InputHandler() = default;
         bool isMouseButtonDown();
-        sf::Vector2i getMousePosition() {return sf::Mouse::getPosition();}
+        sf::Vector2i getMousePosition();
         Static update();
     private:
         sf::Vector2i m_mousePosition;
+        sf::RenderWindow& m_window;
 };
 
 class Breadcrumbs{
--- src/HW2/src/utils.cpp
+++ src/HW2/src/utils.cpp
@@ -1,15 +1,27 @@
 #include "../include/utils.hpp"
+#include <iostream>
+
+InputHandler::InputHandler(sf::RenderWindow& window)
+: m_window(window)
+{
+};
+
 bool InputHandler::isMouseButtonDown(){
     return sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
 };
 
+sf::Vector2i InputHandler::getMousePosition(){
+    return sf::Mouse::getPosition(m_window);
+};
+
 Static InputHandler::update(){
     isMouseButtonDown();
-    getMousePosition();
+    sf::Vector2i mousePosition = getMousePosition();
     Static target;
     if (isMouseButtonDown()){
-        auto targetPos = sf::Vector2f(getMousePosition().x, getMousePosition().y);
+        auto targetPos = sf::Vector2f(mousePosition.x, mousePosition.y);
         target = Static(targetPos,sf::degrees(0.f));
+        // std::cout << "x: " << targetPos.x << "," << targetPos.y << std::endl;
     } else {
         target = Static();
         target.null_output = true;
