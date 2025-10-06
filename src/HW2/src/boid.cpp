0bd85e3e30bcd4d7c73257d220dc1deca8588a14
Prem Gandhi <phgandhi02@gmail.com>
2025-10-06 15:01:34 -0400

    feat(boid): use flee when collision are true
    Collisions with the window are detected and if true then flee is called with the target as the current position and orientation towards window. Boid steering will immediately flee and go within window until conditional is false.

--- src/HW2/src/boid.cpp
+++ src/HW2/src/boid.cpp
@@ -40,21 +40,43 @@ void Boid::update(float dt)
     // Check if the steering output is valid.
     if (m_steering.null_output == false)
     {
+        const bool left_collision = 0 > m_character.getPosition().x;
+        const bool right_collision =  m_character.getPosition().x > m_windowSizeX;
+        const bool up_collision = 0 > m_character.getPosition().y;
+        const bool down_collision =  m_character.getPosition().y > m_windowSizeY;
+
+        auto boid_pos = m_character.getPosition();
+        auto boid_orient = m_character.getOrientation();
+
+        if ((left_collision | right_collision | up_collision | down_collision)) {
+            auto flee_behavior = KinematicFlee();
+            if (left_collision) {
+                flee_behavior.target = Static(sf::Vector2f(boid_pos.x,boid_pos.y),sf::degrees(0.0f));
+            } else if (right_collision) {
+                flee_behavior.target = Static(sf::Vector2f(boid_pos.x,boid_pos.y),sf::degrees(180.0f));
+            } else if (up_collision) {
+                flee_behavior.target = Static(sf::Vector2f(boid_pos.x,boid_pos.y),sf::degrees(90.0f));
+            } else if (down_collision) {
+                flee_behavior.target = Static(sf::Vector2f(boid_pos.x,boid_pos.y),sf::degrees(-90.0f));
+            }
+            m_steering = flee_behavior.getSteering(m_character);
+        }
         // Update the position and orientation
-        m_character.setPosition(m_character.getPosition() + m_steering.getVelocity() * dt);
-        m_character.setOrientation(m_character.getOrientation() + m_steering.getRotation() * dt);
+        m_character.setPosition(boid_pos + m_steering.getVelocity() * dt);
+        m_character.setOrientation(boid_orient + m_steering.getRotation() * dt);
 
         // Wrap orientation between [-pi, pi)
-        if (m_character.getOrientation() > sf::radians(PI_F)) // char.orient > PI
-            m_character.setOrientation(m_character.getOrientation() - sf::radians(2 * PI_F)); // char.orient -= 2*PI
-        else if (m_character.getOrientation() < sf::radians(-PI_F)) // char.orient < -PI
-            m_character.setOrientation(m_character.getOrientation() + sf::radians(2 * PI_F)); // char.orient += 2*PI
+        if (boid_orient > sf::radians(PI_F)) // char.orient > PI
+            m_character.setOrientation(boid_orient - sf::radians(2 * PI_F)); // char.orient -= 2*PI
+        else if (boid_orient < sf::radians(-PI_F)) // char.orient < -PI
+            m_character.setOrientation(boid_orient + sf::radians(2 * PI_F)); // char.orient += 2*PI
 
         // std::cout << "sprite.orient = " << m_character.getOrientationFloat() << std::endl;
+        std::cout << boid_pos.x << " | " << boid_pos.y << std::endl;
         
         // Update the sprite position and rotation
-        m_sprite.setPosition(m_character.getPosition());
-        m_sprite.setRotation(m_character.getOrientation()); // AI-gen: Adjust for sprite facing up
+        m_sprite.setPosition(boid_pos);
+        m_sprite.setRotation(boid_orient); // AI-gen: Adjust for sprite facing up
     } else {
         // std::cout << "No Steering behavior" << std::endl;
     }
@@ -74,6 +96,8 @@ void Boid::draw(sf::RenderWindow &window)
     window.draw(m_sprite);
     m_windowSizeX = window.getSize().x;
     m_windowSizeY = window.getSize().y;
+
+    // std::cout << m_windowSizeX << " | " << m_windowSizeY << std::endl;
 
     for (Breadcrumb crumb : m_breadcrumbs.m_breadcrumbs){
         window.draw(crumb.image);
