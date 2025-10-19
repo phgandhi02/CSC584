ecde58fbc90f8dc58c3eb9a386cca788afbdf543
Prem Gandhi <phgandhi02@gmail.com>
2025-10-18 20:23:06 -0400

    removed unnecessary files for HW3

--- src/HW3/main.cpp
+++ src/HW3/main.cpp
@@ -1,19 +1,6 @@
 // SFML libs
 #include <SFML/Graphics.hpp>
 
-// Boid libs
-#include "./include/boid.hpp"
-#include "./include/boids_colony.hpp"
-
-// Steering behavior libs
-#include "./include/steering/kinematic_align.hpp"
-#include "./include/steering/kinematic_arrive.hpp"
-#include "./include/steering/kinematic_flee.hpp"
-#include "./include/steering/kinematic_seek.hpp"
-#include "./include/steering/kinematic_wander.hpp"
-#include "./include/delegated_steering/kinematic_face.hpp"
-#include "./include/delegated_steering/wander.hpp"
-
 // STL
 #include <memory>
 #include <iostream>
@@ -23,7 +10,7 @@ int main()
 {
     // Seed the random number generator before using random numbers.
     srand(static_cast<unsigned>(time(0)));
-    bool boidsColonyOn = true;
+
     // create window object to render game.
     auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CSC584 HW2: Steering Behaviors");
     window.setFramerateLimit(144); // set the framerate limit to 144 fps.
@@ -32,15 +19,6 @@ int main()
     sf::Texture texture;
     if (!texture.loadFromFile("assets/boid-sm.png")) // make sure the texture loads correctly.
         return EXIT_FAILURE;
-    // sf::Sprite sprite(texture); // create a sprite object to represent the boid in the window.
-    Static startPos = Static(sf::Vector2f(400, 300), sf::degrees(0.0f));
-
-    Boid boid(texture, startPos, window);
-    boid.mouseInputOff = false;
-    auto seek_behavior = std::make_unique<KinematicSeek>();
-    boid.controller = std::move(seek_behavior);
-
-    auto boidsColony = BoidsColony(texture, window, 10.0f, 0.4f, 0.1f, 200);
 
     // Game Loop
     while (window.isOpen())
@@ -53,92 +31,7 @@ int main()
                 window.close();
             }
 
-            if (event->getIf<sf::Event::KeyPressed>())
-            {
-                if (event->getIf<sf::Event::KeyPressed>()->scancode == sf::Keyboard::Scancode::B)
-                {
-                    boidsColonyOn = !boidsColonyOn;
-                    boidsColonyOn ? std::cout << "Boids Colony Toggled On" << std::endl : std::cout << "Boids Colony Toggled Off" << std::endl;
-                    boidsColonyOn ? boid.mouseInputOff = true : boid.mouseInputOff = false;
-                }
-            }
-
-            if (!boidsColonyOn)
-            {
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::I))
-                {
-                    boid.speed *= 1.01f;
-                    std::cout << "New Speed: " << boid.speed << " | Reset to default with \"K\" key" << std::endl;
-                }
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::M))
-                {
-                    boid.speed *= 0.99f;
-                    std::cout << "New Speed: " << boid.speed << " | Reset to default with \"K\" key" << std::endl;
-                }
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::O))
-                {
-                    boid.smoothing *= 1.01f;
-                    std::cout << "New Smoothing: " << boid.smoothing << " | Reset to default with \"K\" key" << std::endl;
-                }
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::U))
-                {
-                    boid.smoothing *= 0.99f;
-                    std::cout << "New Smoothing: " << boid.smoothing << " | Reset to default with \"K\" key" << std::endl;
-                }
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::K))
-                {
-                    boid.speed = 100.f;
-                    boid.smoothing = 0.2f;
-                    std::cout << "Speed set to default: " << boid.speed << std::endl;
-                    std::cout << "Smoothing set to default: " << boid.smoothing << std::endl;
-                }
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Q))
-                {
-                    boid.controller = std::move(std::make_unique<KinematicAlign>());
-                }
-                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W))
-                {
-                    boid.controller = std::move(std::make_unique<KinematicArrive>(5.0f));
-                }
-                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::E))
-                {
-                    boid.controller = std::move(std::make_unique<KinematicFlee>());
-                }
-                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::R))
-                {
-                    boid.controller = std::move(std::make_unique<KinematicSeek>());
-                }
-                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::T))
-                {
-                    boid.controller = std::move(std::make_unique<KinematicWander>());
-                }
-                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::F))
-                {
-                    boid.controller = std::move(std::make_unique<KinematicFace>());
-                }
-                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S))
-                {
-                    boid.controller = std::move(std::make_unique<Wander>(window.getSize().x, window.getSize().y));
-                }
-            }
+            window.display(); // display the new frame.
         }
-        if (!boidsColonyOn)
-        {
-            boid.update(0.01f);
-            boid.draw(window);
-        }
-        if (boidsColonyOn)
-        {
-            boidsColony.update();
-            boidsColony.draw(window);
-        }
-
-        window.display(); // display the new frame.
     }
 }
