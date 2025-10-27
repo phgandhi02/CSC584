2b4587706bc409b1397ba539b1b964b45772e496
Prem Gandhi <phgandhi02@gmail.com>
2025-10-27 14:45:47 -0400

    implemented writing graph objs to cout

--- src/HW3/include/pathfinding_algos.hpp
+++ src/HW3/include/pathfinding_algos.hpp
@@ -5,20 +5,32 @@
 #include "heuristic.hpp"
 
 #include <algorithm>
+#include <iostream>
 
 class PathfindingList
 {
 public:
+    std::ostream &write(std::ostream &os) const
+    {
+        for (auto &record : nodeRecords)
+        {
+            // write stuff to stream
+            os << "Record: " << record << std::endl;
+            return os;
+        }
+    }
     NodeRecord smallestElement();
     bool contains(int node);
     NodeRecord find(int node);
     void add(NodeRecord newRecord);
     void subtract(NodeRecord newRecord);
     int size() { return nodeRecords.size(); }
+    std::vector<NodeRecord> nodeRecords;
 
 private:
-    std::vector<NodeRecord> nodeRecords;
 };
+
+std::ostream &operator<<(std::ostream &os, PathfindingList const &m);
 
 class Pathfinding
 {
--- src/HW3/src/graph.cpp
+++ src/HW3/src/graph.cpp
@@ -1,14 +1,20 @@
 #include "../include/graph.hpp"
 
-std::ostream &operator<<(std::ostream &os, Connection &connection)
+std::ostream &operator<<(std::ostream &os, Connection const &m)
 {
-    os << "FromNode: " << connection.getFromNode() << " | " << "ToNode: " << connection.getToNode() << " | " << "Cost: " << connection.getCost() << std::endl;
-    return os;
-}
+    // since `write` is public, we can call it without any problem.
+    return m.write(os);
+};
+
+std::ostream &operator<<(std::ostream &os, NodeRecord const &m)
+{
+    // since `write` is public, we can call it without any problem.
+    return m.write(os);
+};
 
 std::vector<Connection> Graph::getNodes(int node)
 {
-    std::vector<Connection> connectedNodes = {};
+    std::vector<Connection> connectedNodes = {Connection(node, node, 0.0f)};
     for (auto &connection : graph)
     {
         if (connection.getFromNode() == node && connection.isConnected())
@@ -19,14 +25,14 @@ std::vector<Connection> Graph::getNodes(int node)
     return connectedNodes;
 };
 
-void Graph::addNode(int node)
+void Graph::addNode(int fromNode, int toNode, float cost)
 {
-    auto newConnection = Connection(node);
+    auto newConnection = Connection(fromNode, toNode, cost);
 
     graph.push_back(newConnection);
-}
+};
 
 void Graph::addEdge(Connection connection)
 {
     graph.push_back(connection);
-}
+};
