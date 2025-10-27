af96ed7a63bbd923d75a54c4a7d200debd3f2ffa
Prem Gandhi <phgandhi02@gmail.com>
2025-10-27 14:45:29 -0400

    implement main script logic

--- src/HW3/app/graph_analysis.cpp
+++ src/HW3/app/graph_analysis.cpp
@@ -1,17 +1,23 @@
+#include "../include/graph.hpp"
 #include "../include/pathfinding_algos.hpp"
 
 int main()
 {
     std::cout << "Graph Analysis Running!" << std::endl;
-    // Graph graph = Graph();
-    // graph.addEdge(Connection(1, 2, 1.3f));
-    // graph.addEdge(Connection(1, 3, 1.6f));
-    // graph.addEdge(Connection(1, 4, 3.3f));
-    // graph.addEdge(Connection(2, 5, 1.5f));
-    // graph.addEdge(Connection(2, 6, 1.9f));
-    // graph.addEdge(Connection(3, 4, 1.3f));
-    // graph.addEdge(Connection(6, 7, 1.4f));
-    // auto pathfinding = Pathfinding();
-    // pathfinding.DijkstraAlgorithm(graph, 1, 7);
+    Graph graph = Graph();
+    graph.addEdge(Connection(1, 2, 1.3f));
+    graph.addEdge(Connection(1, 3, 1.6f));
+    graph.addEdge(Connection(1, 4, 3.3f));
+    graph.addEdge(Connection(2, 5, 1.5f));
+    graph.addEdge(Connection(2, 6, 1.9f));
+    graph.addEdge(Connection(3, 4, 1.3f));
+    graph.addEdge(Connection(6, 7, 1.4f));
+    auto pathfinding = Pathfinding();
+    auto path = pathfinding.DijkstraAlgorithm(graph, 1, 7);
+    for (auto &connection : path)
+    {
+        std::cout << connection << std::endl;
+    }
+    std::cout << "Graph Analysis Complete!" << std::endl;
     return 0;
 }
