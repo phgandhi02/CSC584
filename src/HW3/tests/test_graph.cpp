912e57476871af2896359ad72c051ddc2fc55ad3
Prem Gandhi <phgandhi02@gmail.com>
2025-10-27 10:15:02 -0400

    implemented basic test for dijkstra's algorithm

--- src/HW3/tests/test_graph.cpp
+++ src/HW3/tests/test_graph.cpp
@@ -1,4 +1,7 @@
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+
+#include "../include/pathfinding_algos.hpp"
+
 /*
 - Anything can be asserted as long as it can be streamed to an ostream object, in particular C-strings and string objects.
 
@@ -15,22 +18,32 @@ To create a fixture you must: derive a class from the testing::Test class and st
 The rule of thumb is to use EXPECT_* when you want the test to continue to reveal more errors after the assertion failure, and use ASSERT_* when continuing after failure doesn’t make sense. For example, the second assertion in the Dequeue test is ASSERT_NE(n, nullptr), as we need to dereference the pointer n later, which would lead to a segfault when n is NULL.
 
 */
-class GraphTest : public testing::Test
+
+class DijkstraAlgorithmTest : public testing::Test
 {
 protected:
-    int var1 = 0;
+    DijkstraAlgorithmTest()
+    {
+        graph_.addEdge(Connection(1, 2, 1.3f));
+        graph_.addEdge(Connection(1, 3, 1.6f));
+        graph_.addEdge(Connection(1, 4, 3.3f));
+        graph_.addEdge(Connection(2, 5, 1.5f));
+        graph_.addEdge(Connection(2, 6, 1.9f));
+        graph_.addEdge(Connection(3, 4, 1.3f));
+        graph_.addEdge(Connection(6, 7, 1.4f));
+        pathfinding_.DijkstraAlgorithm(graph_, 1, 7);
+    }
+    Graph graph_;
+    Pathfinding pathfinding_;
 };
 
-TEST_F(GraphTest, GraphFixtureTest){
-
-};
-
-/*
-Graph Initization Test.
-
-
-*/
-TEST(Graph, GraphInitialization)
+TEST_F(DijkstraAlgorithmTest, AddEdgeWorks)
 {
-    EXPECT_EQ(0, 0);
+    const auto nodesAdjacentToNode1 = graph_.getNodes(1);
+    const std::vector<Connection> expectedPath = {Connection(1, 2, 1.3),
+                                                  Connection(1, 6, 1.9),
+                                                  Connection(6, 7, 1.4)};
+
+    bool graphsMatch = (nodesAdjacentToNode1 == expectedPath);
+    EXPECT_TRUE(graphsMatch);
 }
