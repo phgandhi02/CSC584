02d7b03f9ce19b4034fa65fc5e929e02a96b0c9a
Prem Gandhi <phgandhi02@gmail.com>
2025-11-01 13:02:04 -0400

    fix: removed duplicate << operator overload

--- src/HW3/src/graph.cpp
+++ src/HW3/src/graph.cpp
@@ -6,12 +6,6 @@ std::ostream &operator<<(std::ostream &os, Connection const &m)
     return m.write(os);
 };
 
-std::ostream &operator<<(std::ostream &os, NodeRecord const &m)
-{
-    // since `write` is public, we can call it without any problem.
-    return m.write(os);
-};
-
 std::vector<Connection> Graph::getNodes(int node)
 {
     std::vector<Connection> connectedNodes = {Connection(node, node, 0.0f)};
