cabe1cddd6e520386cf7b6276e48e307574f62ad
Prem Gandhi <phgandhi02@gmail.com>
2025-11-01 20:12:13 -0400

    shifted game setup to separate function.

--- src/HW3/app/sfml_demo.cpp
+++ src/HW3/app/sfml_demo.cpp
@@ -13,36 +13,8 @@
 #include <iostream>
 #include <random>
 
-int main()
+void boid_movement(Boid &boid, sf::Window &window)
 {
-    // Seed the random number generator before using random numbers.
-    srand(static_cast<unsigned>(time(0)));
-    // create window object to render game.
-    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CSC584 HW2: Steering Behaviors");
-    window.setFramerateLimit(144); // set the framerate limit to 144 fps.
-
-    // create a texture object to load the boid image.
-    sf::Texture texture;
-    if (!texture.loadFromFile("/home/prem/code/CSC584/src/HW3/assets/boid-sm.png")) // make sure the texture loads correctly.
-        return EXIT_FAILURE;
-    // sf::Sprite sprite(texture); // create a sprite object to represent the boid in the window.
-    Static startPos = Static(sf::Vector2f(400, 300), sf::degrees(0.0f));
-
-    Boid boid(texture, startPos, window);
-    auto seek_behavior = std::make_unique<KinematicSeek>();
-    boid.controller = std::move(seek_behavior);
-
-    // Game Loop
-    while (window.isOpen())
-    {
-        window.clear(sf::Color::White);                        // clear the window with a white background.
-        while (const std::optional event = window.pollEvent()) // event polling loop.
-        {
-            if (event->is<sf::Event::Closed>()) // If user closes the window, close the application.
-            {
-                window.close();
-            }
-
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::I))
             {
                 boid.speed *= 1.01f;
