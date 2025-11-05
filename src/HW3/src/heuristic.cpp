d6ef09620bcce1ff9b0ae7ef7e398830d737e0d4
Prem Gandhi <phgandhi02@gmail.com>
2025-11-05 08:37:12 -0500

    Euclidean distance heuristic calculate distance from coordinates instead of simple subtraction

--- src/HW3/src/heuristic.cpp
+++ src/HW3/src/heuristic.cpp
@@ -2,10 +2,12 @@
 
 float EuclidianHeuristic::estimate(int fromNode)
 {
-    return (goalNode - fromNode);
+    // return (goalNode - fromNode);
+    return sf::Vector2f(calculatePositionfromNode(fromNode) - calculatePositionfromNode(goalNode)).length();
 }
 
 float EuclidianHeuristic::estimate(int fromNode, int goalNode)
 {
-    return (goalNode - fromNode);
+    // return (goalNode - fromNode);
+    return sf::Vector2f(calculatePositionfromNode(fromNode) - calculatePositionfromNode(goalNode)).length();
 }
