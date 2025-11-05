39f1aba1b8cf074e9635da5f4515d4d4ad1bcede
Prem Gandhi <phgandhi02@gmail.com>
2025-11-05 13:22:49 -0500

    removed unnessary boid movement function and cleanup

--- src/HW3/app/sfml_demo.cpp
+++ src/HW3/app/sfml_demo.cpp
@@ -14,77 +14,6 @@
 #include <iostream>
 #include <random>
 
-void boid_movement(Boid &boid, sf::Window &window)
-{
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::I))
-    {
-        boid.speed *= 1.01f;
-        std::cout << "New Speed: " << boid.speed << " | Reset to default with \"K\" key" << std::endl;
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::M))
-    {
-        boid.speed *= 0.99f;
-        std::cout << "New Speed: " << boid.speed << " | Reset to default with \"K\" key" << std::endl;
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::O))
-    {
-        boid.smoothing *= 1.01f;
-        std::cout << "New Smoothing: " << boid.smoothing << " | Reset to default with \"K\" key" << std::endl;
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::U))
-    {
-        boid.smoothing *= 0.99f;
-        std::cout << "New Smoothing: " << boid.smoothing << " | Reset to default with \"K\" key" << std::endl;
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::K))
-    {
-        boid.speed = 100.f;
-        boid.smoothing = 0.2f;
-        std::cout << "Speed set to default: " << boid.speed << std::endl;
-        std::cout << "Smoothing set to default: " << boid.smoothing << std::endl;
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Q))
-    {
-        boid.controller = std::move(std::make_unique<KinematicAlign>());
-        std::cout << "Boid Aligning" << std::endl;
-    }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W))
-    {
-        boid.controller = std::move(std::make_unique<KinematicArrive>(0.5f));
-        std::cout << "Boid Arriving" << std::endl;
-    }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::E))
-    {
-        boid.controller = std::move(std::make_unique<KinematicFlee>());
-        std::cout << "Boid Fleeing" << std::endl;
-    }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::R))
-    {
-        boid.controller = std::move(std::make_unique<KinematicSeek>());
-        std::cout << "Boid Seeking" << std::endl;
-    }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::T))
-    {
-        boid.controller = std::move(std::make_unique<KinematicWander>());
-        std::cout << "Boid Kinematic Wander" << std::endl;
-    }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::F))
-    {
-        boid.controller = std::move(std::make_unique<KinematicFace>());
-        std::cout << "Boid Kinematic Face" << std::endl;
-    }
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S))
-    {
-        boid.controller = std::move(std::make_unique<Wander>(window.getSize().x, window.getSize().y));
-        std::cout << "Boid Wander" << std::endl;
-    }
-};
-
 void draw_map(std::array<std::array<Cell, MAP_WIDTH>, MAP_HEIGHT> map, sf::RenderWindow &window)
 {
     auto cell_shape = sf::RectangleShape(sf::Vector2f(CELL_SIZE, CELL_SIZE));
@@ -224,7 +153,7 @@ int main()
                 window.close();
             }
 
-            boid_movement(boid, window); // change boid movement algorithm based on keyboard input
+            // boid_movement(boid, window); // change boid movement algorithm based on keyboard input
         }
         boid_position = boid.getPosition(); // store boid position
 
@@ -283,10 +212,10 @@ int main()
                 boid.setTarget(target);
             }
         }
-        std::cout << std::endl;
-        std::cout << "Boid: " << boid_position.x << " | " << boid_position.y << " | " << currentNode << std::endl;
-        std::cout << "Mouse: " << mouse.getPosition().x << " | " << mouse.getPosition().y << std::endl;
-        std::cout << "Target: " << targetPos.x << " | " << targetPos.y << " | " << targetNode << std::endl;
+        // std::cout << std::endl;
+        // std::cout << "Boid: " << boid_position.x << " | " << boid_position.y << " | " << currentNode << std::endl;
+        // std::cout << "Mouse: " << mouse.getPosition().x << " | " << mouse.getPosition().y << std::endl;
+        // std::cout << "Target: " << targetPos.x << " | " << targetPos.y << " | " << targetNode << std::endl;
 
         // update the boid position.
         boid.update(0.01f);
--- src/HW3/compile_and_test.sh
+++ src/HW3/compile_and_test.sh
@@ -1,4 +1,4 @@
 cmake -S . -B build
 cmake --build build
-./build/bin/graph_analysis
 ./build/bin/sfml_demo
+./build/bin/graph_analysis
