d4d45da537e46933e3258298428e615228493d44
Prem Gandhi <phgandhi02@gmail.com>
2025-11-29 15:52:22 -0500

    created main file as placeholder for program entrypoint

---
+++ src/HW4/main.cpp
@@ -0,0 +1,19 @@
+#include "./include/global.hpp"
+#include "./include/pathfinding/graph.hpp"
+#include "./include/pathfinding/pathfinding_algos.hpp"
+
+// Boid libs
+#include "./include/movement/boid.hpp"
+
+// SFML libs
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+// STL
+#include <memory>
+#include <iostream>
+#include <random>
+
+int main(){
+    return 0;
+}
