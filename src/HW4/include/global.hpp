0e8ceeaa8d2000868a81a74175db4ee0c2525e64
Prem Gandhi <phgandhi02@gmail.com>
2025-12-02 17:58:39 -0500

    update Cell enum to match values

--- src/HW4/include/global.hpp
+++ src/HW4/include/global.hpp
@@ -5,10 +5,6 @@ constexpr unsigned int MAP_HEIGHT = 40;
 constexpr unsigned int MAP_WIDTH = 40;
 constexpr unsigned int CELL_SIZE = 20;
 
-enum Cell
-{
-    Empty,
-    Wall
-};
+enum Cell {Empty = ' ', Wall = '#' };
 
 #endif // GLOBAL_HPP
