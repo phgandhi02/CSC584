5c42c3965b88d298c554b2fd7803eeb31d75bac8
Prem Gandhi <phgandhi02@gmail.com>
2025-12-02 10:40:41 -0500

    implemented main game logic for Window

--- src/HW4/main.cpp
+++ src/HW4/main.cpp
@@ -8,12 +8,36 @@
 // SFML libs
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 
 // STL
-#include <memory>
 #include <iostream>
+#include <memory>
 #include <random>
 
-int main(){
-    return 0;
+int main() {
+    /* -------------------------------------------------------------------------- */
+    /*                               Game Loop Setup                              */
+    /* -------------------------------------------------------------------------- */
+
+    // window creates Window obj. Must include event handling loop to ensure
+    // the program doesn't end immediately. 
+    // * use sf::VideoMode to get desktop resolution for dynamic sizing
+    sf::Window window(sf::VideoMode({800, 600}), "CSC584 HW4"); 
+
+    /* -------------------------------------------------------------------------- */
+    /*                               Main Game Loop                               */
+    /* -------------------------------------------------------------------------- */
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last
+        // iteration of the loop
+        while (const std::optional event = window.pollEvent()) {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                // Implement any logic needed before closing game ie. save game etc.
+                window.close();
+            
+            /* ------------------------- Check for Input Events ------------------------- */
+        }
+    }
 }
