c2b18b7683bfa3e15bb601b7a4278a510caafa8c
Prem Gandhi <phgandhi02@gmail.com>
2025-12-03 10:07:29 -0500

    feat(main): :sparkles: draw enemy sprite in static location

--- src/HW4/main.cpp
+++ src/HW4/main.cpp
@@ -123,6 +123,12 @@ int main() {
     auto seedTexture = sf::Texture("seeds.png");
     sf::Sprite seed(seedTexture);
     seed.scale(sf::Vector2f(.3,.3));
+
+    // Create enemy sprite
+    auto enemyCatTexture = sf::Texture("chasingCat.png");
+    sf::Sprite enemyCat(enemyCatTexture);
+    enemyCat.scale(sf::Vector2f(.3,.3));
+    enemyCat.setPosition(sf::Vector2f(100,100));
     /* -------------------------------------------------------------------------- */
     /*                               Main Game Loop                               */
     /* -------------------------------------------------------------------------- */
@@ -147,6 +153,7 @@ int main() {
 
         // draw on window
         draw_map(map, window); // draw map
+        window.draw(enemyCat);
 
         i++;
         if ((i % 100) == 0)
