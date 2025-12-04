42f565bd3c5c9bd22cf0e306fb8772bfaf357d56
Prem Gandhi <phgandhi02@gmail.com>
2025-12-04 11:51:15 -0500

    implemented CharacterState as data struct for GameState

--- src/HW4/include/decision-making/game_state.hpp
+++ src/HW4/include/decision-making/game_state.hpp
@@ -3,14 +3,35 @@
 
 #include <SFML/System.hpp>
 
+class CharacterState
+{
+public:
+    CharacterState() = default;
+    CharacterState(Boid& boid) {
+        character = boid.getCharacter();
+        textureRect = boid.getTextureRect();
+        target = boid.getTarget();
+        steeringOutput = boid.getSteering();
+    }
+    ~CharacterState() = default;
+    sf::Vector2f getPosition() { return character.getPosition(); }
+    Static character;
+    sf::IntRect textureRect;
+    Static target;
+    KinematicSteeringOutput steeringOutput;
+};
+
 class GameState
 {
 public:
-    GameState(sf::Vector2f enemyCat, sf::Vector2f player, sf::Vector2f seed, float score): 
-        m_enemyCatPos(enemyCat), m_playerPos(player), m_seedPos(seed), m_score(score) {};
+    GameState(Boid& enemyCat, Boid& player, Boid seed, float score)
+    {
+        m_enemyCat = CharacterState(enemyCat);
+        m_player = CharacterState(player);
+    }
     ~GameState() = default;
-    sf::Vector2f m_enemyCatPos;
-    sf::Vector2f m_playerPos;
+    CharacterState m_enemyCat;
+    CharacterState m_player;
     sf::Vector2f m_seedPos;
     float m_score;
 };
