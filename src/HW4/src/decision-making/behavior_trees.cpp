95c8278931dd32af8636efbaede90a8dd59c5ea2
Prem Gandhi <phgandhi02@gmail.com>
2025-12-05 11:38:29 -0500

    implement run func for BT subclasses

--- src/HW4/src/decision-making/behavior_trees.cpp
+++ src/HW4/src/decision-making/behavior_trees.cpp
@@ -1,1 +1,38 @@
-#include "../../include/decision-making/decision_trees.hpp"
+#include "../../include/decision-making/behavior_trees.hpp"
+
+Status Selector::run(DecisionContext context)
+{
+    for (const auto& child : m_children)
+    {
+        auto childStatus = child->run(context);
+        if (childStatus == SUCCESS || childStatus == RUNNING)
+        {
+            return childStatus;
+        }
+    }
+    return FAIL;
+}
+
+Status Sequence::run(DecisionContext context)
+{
+    for (const auto& child : m_children)
+    {
+        auto childStatus = child->run(context);
+        if (childStatus == FAIL || childStatus == RUNNING)
+        {
+            return childStatus;
+        }
+    }
+    return SUCCESS;
+
+}
+
+Status Inverter::run(DecisionContext context)
+{
+    switch(m_child->run(context)){
+      case FAIL: return SUCCESS;
+      case SUCCESS: return FAIL;
+      default: return RUNNING;
+    }
+}
+
