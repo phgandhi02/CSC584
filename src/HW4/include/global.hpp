ce3440a0d7e0f286e773913aee7140cc099ff1b8
Prem Gandhi <phgandhi02@gmail.com>
2025-12-05 23:41:01 -0500

    fixed RandNumGen initialization to be seeded properly

--- src/HW4/include/global.hpp
+++ src/HW4/include/global.hpp
@@ -43,9 +43,10 @@ public:
   RandomNumGen(int lower_limit, int higher_limit)
       : m_gen(), m_distrib(static_cast<int>(lower_limit),
                            static_cast<int>(higher_limit)) {
-    // ;  // a seed source for the random number engine
-    // std::mt19937 this->m_gen(rd()); // mersenne_twister_engine seeded with
-    // rd() std::uniform_int_distribution<> this->m_distrib();
+    // a seed source for the random number engine
+    std::random_device rd;
+    auto m_gen(rd()); // mersenne_twister_engine seeded with
+    auto m_distrib(m_gen);
   }
   ~RandomNumGen() = default;
   int getRandomInt() { return m_distrib(m_gen); }
