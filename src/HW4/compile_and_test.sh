278e5a69d4dc9d79b6d97bbff3bd4ccb6fb8ef21
Prem Gandhi <phgandhi02@gmail.com>
2025-12-11 14:20:01 -0500

    export compile commands for clang-tidy check

--- src/HW4/compile_and_test.sh
+++ src/HW4/compile_and_test.sh
@@ -1,4 +1,4 @@
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build
 cd build && ctest --output-on-failure
 # ./build/bin/main
